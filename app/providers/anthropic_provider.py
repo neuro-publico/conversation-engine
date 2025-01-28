32458f744623a5650439597e1c6e4ca9d8bb6897
Oscar Arellano <arellanooscar.ing@gmail.com>
2025-01-27 20:16:40 -0500

    comment

--- app/providers/anthropic_provider.py
+++ app/providers/anthropic_provider.py
@@ -6,7 +6,7 @@ class AnthropicProvider(AIProviderInterface):
     def get_llm(self, model: str, temperature: float, max_tokens: int, top_p: int) -> ChatAnthropic:
         return ChatAnthropic(
             model=model,
-            temperature=temperature,
-            max_tokens=max_tokens,
-            top_p=top_p
+            #temperature=temperature,
+            #max_tokens=max_tokens,
+            #top_p=top_p
         )
