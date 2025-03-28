fbe13826bc0ba22a7452bdb47b067de86c37dfbf
Oscar Arellano <arellanooscar.ing@gmail.com>
2025-03-28 15:56:46 -0500

    generate images from
    generate images from

--- app/services/image_service_interface.py
+++ app/services/image_service_interface.py
@@ -1,9 +1,14 @@
 from abc import abstractmethod, ABC
 
+from app.requests.generate_image_request import GenerateImageRequest
 from app.requests.variation_image_request import VariationImageRequest
 
 
 class ImageServiceInterface(ABC):
     @abstractmethod
     async def generate_variation_images(self, request: VariationImageRequest, owner_id: str):
+        pass
+
+    @abstractmethod
+    async def generate_images_from(self, request: GenerateImageRequest, owner_id: str):
         pass
