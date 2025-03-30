b9d5ee0533628b9416ae2abe5056bf1e94023ce3
Oscar Arellano <arellanooscar.ing@gmail.com>
2025-03-29 23:09:03 -0500

    add logic for receive a url image.

--- app/requests/generate_image_request.py
+++ app/requests/generate_image_request.py
@@ -4,5 +4,6 @@ from typing import Optional
 
 class GenerateImageRequest(BaseModel):
     file: Optional[str] = None
+    file_url: Optional[str] = None
     prompt: str
     num_variations: int = 4
